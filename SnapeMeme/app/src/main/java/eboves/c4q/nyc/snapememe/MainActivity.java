9074b6f99767c6f2e3f11416236f96e4b1897f54
eboves <eboves@gmail.com>
2015-06-03 23:19:09 -0400

    New way to take the photo

--- SnapeMeme/app/src/main/java/eboves/c4q/nyc/snapememe/MainActivity.java
+++ SnapeMeme/app/src/main/java/eboves/c4q/nyc/snapememe/MainActivity.java
@@ -2,49 +2,86 @@ package eboves.c4q.nyc.snapememe;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+
 
 public class MainActivity extends ActionBarActivity {
 
     private Button buttonCam;
-    private Uri fileUri;
     private ImageView imageHold;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/myImage.jpg");
         buttonCam = (Button) findViewById(R.id.buttonCamera);
         imageHold = (ImageView) findViewById(R.id.imageHolder);
+        displayCameraBitmap();
+
     }
 
         public void takePhoto(View view) {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+                Uri outputFileUri = Uri.fromFile(file);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageHold.setImageBitmap(imageBitmap);
+            displayCameraBitmap();
 
+        }
+    }
 
-          }
+    private void displayCameraBitmap() {
+        int degree = 0;
+        try {
+            ExifInterface exifInterface = new ExifInterface(file.getAbsolutePath());
+            int orientation =   exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0);
+            Log.d("mainActivity", "orientation " + orientation);
+            switch (orientation){
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+        imageHold.setImageDrawable(new FakeBitmapDrawable(bitmap, degree));
+        Log.d("MainActivity", Uri.fromFile(file).toString());
+
     }
 
 
