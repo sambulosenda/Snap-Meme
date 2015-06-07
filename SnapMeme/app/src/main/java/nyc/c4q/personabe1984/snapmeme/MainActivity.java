c0f907f21a76401da3ceac55d034c950b703b406
personabe1984 <personabe1984@gmail.com>
2015-06-07 11:05:44 -0400

    Made a method that will able us to post pictures

--- SnapMeme/app/src/main/java/nyc/c4q/personabe1984/snapmeme/MainActivity.java
+++ SnapMeme/app/src/main/java/nyc/c4q/personabe1984/snapmeme/MainActivity.java
@@ -2,16 +2,18 @@ package nyc.c4q.personabe1984.snapmeme;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.facebook.FacebookSdk;
 
 
 public class MainActivity extends Activity {
-
+    Intent intent = null, chooser = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +25,17 @@ public class MainActivity extends Activity {
         shareIntent.setType("image/jpeg");
         //startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.send_to)));
     }
-
+    public void process(View view){
+        if(view.getId() == R.id.sendImage){
+            Uri imageUri = Uri.parse("android.resource://com.example.android.implicitintentexample/drawable" + R.mipmap.ic_launcher);
+            intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_STREAM,imageUri);
+            intent.putExtra(Intent.EXTRA_TEXT,"Hey I have attached this picture");
+            chooser = Intent.createChooser(intent,"Send Picture");
+            startActivity(chooser);
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
